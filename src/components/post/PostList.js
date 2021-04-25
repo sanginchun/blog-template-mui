52d6127d92bd300732d18cca41e491d265520f4f
sanginchun <sanginchun91@gmail.com>
2021-04-25 15:56:05 +0900

    feat: Pagination

--- src/components/post/PostList.js
+++ src/components/post/PostList.js
@@ -1,20 +1,41 @@
 import { Grid } from "@material-ui/core";
+import { Pagination } from "@material-ui/lab";
 import React from "react";
 
 import PostExcerpt from "./PostExcerpt";
 import { posts } from "./dummyPosts";
 
+const POST_PER_PAGE = 8;
+
 function PostList() {
-  const renderedPostList = posts.map((post, index) => (
-    <Grid item key={index}>
-      <PostExcerpt post={post} />
-    </Grid>
-  ));
+  const [page, setPage] = React.useState(1);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+
+  const postStart = (page - 1) * POST_PER_PAGE;
+
+  const renderedPostList = posts
+    .slice(postStart, postStart + POST_PER_PAGE)
+    .map((post, index) => (
+      <Grid item key={index}>
+        <PostExcerpt post={post} />
+      </Grid>
+    ));
 
   return (
-    <Grid container>
+    <Grid container justify="center" spacing={1}>
       <Grid item container xs={12}>
         {renderedPostList}
+      </Grid>
+      <Grid item>
+        <Pagination
+          count={Math.ceil(posts.length / POST_PER_PAGE)}
+          page={page}
+          onChange={handleChange}
+          color="primary"
+          size="large"
+        />
       </Grid>
     </Grid>
   );
